a66836b4d54cc4031509bc659abd99cae2de630f
YunusStahlschmidt <stahlschmidt.yunus@gmail.com>
2021-08-02 23:59:36 +0300

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Qimia Gatsby",
+  },
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
+};
